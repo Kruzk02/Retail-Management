1c282f64eb8b4f899f4a838622af270c8828e1a9
Thien phuc <thienphuc484@gmail.com>
2025-04-13 11:36:11 +0700

    feat: add properties file

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -1,12 +1,36 @@
 package org;
 
+import org.env.DotenvPropertySource;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(AppConfig.class);
-        var helloService = context.getBean(HelloService.class);
+        var context = new AnnotationConfigApplicationContext();
 
-        System.out.println(helloService.hello());
+        try {
+            DotenvPropertySource dotenv = new DotenvPropertySource("dotenv");
+            ConfigurableEnvironment env = context.getEnvironment();
+            env.getPropertySources().addFirst(dotenv);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load .env", e);
+        }
+
+        context.register(AppConfig.class);
+        context.refresh();
+
+        var template = context.getBean(JdbcTemplate.class);
+        Integer result = template.queryForObject("SELECT 1", Integer.class);
+        if (result != null && result > 0) {
+            System.out.println("Database is up");
+        } else {
+            System.out.println("Database is down");
+        }
     }
 }
