12c1f51092dda6cd4a3b90e7cc25b6a531420fae
Thien phuc <thienphuc484@gmail.com>
2025-04-17 10:15:56 +0700

    feat(config): add DaoConfig to AppConfig

--- src/main/java/org/config/AppConfig.java
+++ src/main/java/org/config/AppConfig.java
@@ -4,6 +4,5 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ ServiceConfig.class, DatabaseConfig.class, SecurityConfig.class })
-public class AppConfig {
-}
+@Import({ DaoConfig.class, ServiceConfig.class, DatabaseConfig.class, SecurityConfig.class })
+public class AppConfig { }
