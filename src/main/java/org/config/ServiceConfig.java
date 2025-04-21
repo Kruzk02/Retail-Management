4eb12ba9d5309126fd5386c6163f6580eba3e0c4
Thien phuc <thienphuc484@gmail.com>
2025-04-21 11:36:23 +0700

    feat(config): add UserService to ServiceConfig

--- src/main/java/org/config/ServiceConfig.java
+++ src/main/java/org/config/ServiceConfig.java
@@ -1,8 +1,17 @@
 package org.config;
 
+import org.dao.UserDao;
+import org.service.UserService;
+import org.service.impl.UserServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ServiceConfig {
 
+    @Bean
+    public UserService userService(UserDao userDao, PasswordEncoder passwordEncoder) {
+        return new UserServiceImpl(userDao, passwordEncoder);
+    }
 }
