1eb5761d8d39a697d9bd3cb3aca0880bd76aa382
Thien phuc <thienphuc484@gmail.com>
2025-04-30 10:37:09 +0700

    refactor(Product): change from int to Integer in quantity field

--- src/main/java/org/model/Product.java
+++ src/main/java/org/model/Product.java
@@ -19,7 +19,7 @@ public class Product {
     private String name;
     private String description;
     private BigDecimal price;
-    private int quantity;
+    private Integer quantity;
     private Collection<Category> categories;
     private Timestamp created_at;
 
