750d2708c0747e59f8213a3d5066e7e4e2ec40af
Thien phuc <thienphuc484@gmail.com>
2025-05-21 10:37:43 +0700

    feat(validators): add LoginRequestValidator

--- src/main/java/org/dto/LoginRequest.java
+++ src/main/java/org/dto/LoginRequest.java
@@ -1,14 +1,3 @@
 package org.dto;
 
-public record LoginRequest(String username, String password) {
-
-    public LoginRequest {
-        if (username == null || username.isBlank()) {
-            throw new IllegalArgumentException("Username cannot be blank");
-        }
-
-        if (password == null || password.isBlank()) {
-            throw new IllegalArgumentException("Password cannot be blank");
-        }
-    }
-}
+public record LoginRequest(String username, String password) { }
